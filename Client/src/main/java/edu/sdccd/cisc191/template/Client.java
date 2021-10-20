fed3390f741b39f0cbb84b8e4b0eade13162fccf
Arisachi Tanaka <baotanaka@hotmail.co.jp>
2021-10-20 03:44:03 -0700

    Initial Commit

--- Client/src/main/java/edu/sdccd/cisc191/template/Client.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/Client.java
@@ -18,38 +18,42 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /* v.1.0.3 update: This JavaFX on Client allows user to interact in the way of inputting, pressing
- * and the output. We are going to import a lot of javafx stuff
+ * and the output. We are going to import a lot of javafx stuff.
+ *  It is for the beginning of the project where the user first interacts with the app.
  */
 
 public class Client extends Application{ // extend application is how JavaFX starts always
 
-    private Parent createContent(){
-        VBox root = new VBox();
-        root.setPrefSize(500,500);
+    private Parent createContent(){ //how to create a "parent" to set the GUI
 
-        TextField input=new TextField();
-        input.setFont(Font.font(18));
+        VBox root = new VBox(); //sets and defines the children which will lay it out vertically
+        root.setPrefSize(500,500); //its size
 
-        Text output = new Text();
-        output.setFont(Font.font(18));
+        TextField input=new TextField(); //set store and define the input text
+        input.setFont(Font.font(20)); //set the font of the user input
 
-        Button button= new Button ("Start here");
+        Text output = new Text(); //set store and define the output which will be just printin gout the input
+        output.setFont(Font.font(20)); //set the font of the output
 
-        button.setOnAction(e-> { output.setText(input.getText());});
-        root.getChildren().addAll( input, button, output);
+        Button button= new Button ("Start here"); //button for the user to press after typing
+
+        button.setOnAction(e-> { output.setText(input.getText());}); //after the  press this makes sure that output will be whatever the input
+        root.getChildren().addAll( input, button, output); //type input, press button, output emerges
 
         return root;
     }
 
-    @Override
+    @Override //method override
 
     public void start(Stage stage) throws Exception{
         stage.setScene(new Scene(createContent()));
         stage.show();
+        //start setup of stage for the GUI
     }
 
     public static void main(String[] args){
         launch (args);
+        //launch
     }
 
 } //end class Client
