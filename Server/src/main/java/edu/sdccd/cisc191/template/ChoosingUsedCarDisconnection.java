b94520636e5e1881d2e031c8cf58f1f15df62878
Arisachi Tanaka <baotanaka@hotmail.co.jp>
2021-12-11 18:33:40 -0800

    Initial Commit

--- Server/src/main/java/edu/sdccd/cisc191/template/ChoosingUsedCarDisconnection.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/ChoosingUsedCarDisconnection.java
@@ -1,8 +1,10 @@
 package edu.sdccd.cisc191.template;
 //v.14.5.0 used lambdas to finish  connection (Server end)
 
-public class ChoosingUsedCarDisconnection {
 
+//v.14.5.1 concurrency/multithreading to further facilitate the disconnection
+public class ChoosingUsedCarDisconnection implements Runnable {
+    //going to implement runnable
     public static void main(String[] args){
 
         String name="Okay come back next time";
@@ -13,6 +15,23 @@ public class ChoosingUsedCarDisconnection {
         };
 
         myInterface.message(name);
+    }
+
+    //start of the runnable implementation the multithreading part
+    @Override
+
+    public void run(){
+
+        for (int i=1; i<=3; i++){
+            System.out.println("Okay come back next time");
+
+            try{
+                Thread.sleep(2000);
+                //when  looping through make it pause forr 2 sec
+            }catch(InterruptedException e){
+
+            }
+        }
     }
 //this ChoosingUsedCarDisconnection class is where lambda is actually used. The name of the string
     // is defined as "Okay come back next time" and ServerConnectionEnd, which was the user-defined interface
--- Server/src/main/java/edu/sdccd/cisc191/template/ServerConnectionEnd.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/ServerConnectionEnd.java
@@ -2,9 +2,22 @@ package edu.sdccd.cisc191.template;
 
 
 //v.14.5.0 used lambdas to finish  connection (server end)
+//v.14.5.1 concurrency/multithreading to further facilitate the disconnection
 
 public interface ServerConnectionEnd {
     public void message(String name);
+
+    //the multithreading part
+    public static void main (String[] args){
+
+        ChoosingUsedCarDisconnection end=new ChoosingUsedCarDisconnection();
+        ChoosingUsedCarDisconnection end2=new ChoosingUsedCarDisconnection();
+
+        end.run();
+        end2.run();
+
+        //we are going to thread twice. So it will print out 2x3=6 times.
+    }
     //this part establishes the ending message. It is a functional interface
     //which I defined as ServerConnectionEnd, with intent to print string message called known as name
 }
